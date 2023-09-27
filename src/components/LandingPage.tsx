628c974cd9b303b0215066f4b0ef62362784c75e
Angelina <linaemalmros@gmail.com>
2023-09-27 12:02:14 +0200

    added TODO comments

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,7 +1,12 @@
+import NavBar from "../common/NavBar"
+
 function LandingPage() {
 
     return (
         <>
+        <NavBar />
+        {/* TODO: add images and witty description of company and their services */}
+        {/* TODO: add footer component? */}
         </>
     )
 }
