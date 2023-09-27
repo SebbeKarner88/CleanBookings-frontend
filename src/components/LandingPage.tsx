1f4808b7701c0b81ed9c04a7b96d175c39169a69
Angelina <linaemalmros@gmail.com>
2023-09-27 14:06:38 +0200

    added a quote feature for fun

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,7 @@
 import NavBar from "../common/NavBar"
 import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
 import sponge from '../assets/images/sponge.jpg'
 
 function LandingPage() {
@@ -10,10 +11,25 @@ function LandingPage() {
             <NavBar />
             {/* TODO: add images and witty description of company and their services */}
             {/* TODO: add footer component? */}
-            <p>Tjo bror</p>
-            <Container>
+
+            <Container className="mt-5">
                 <Image src={sponge} fluid />
             </Container>
+
+            <Card className="mt-5">
+                <Card.Header>What's Been Said About Us</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {' '}
+                            Här sparas det inte på dyra rengöringsmedel!{' '}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Sven Svensson, <cite title="Source Title">A Happy Client</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
         </>
     )
 }
