cc6a2677380bc95c6c259805bc2a91e151f9c40f
Angelina <linaemalmros@gmail.com>
2023-09-27 13:58:22 +0200

    playing around with image

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,13 +1,19 @@
 import NavBar from "../common/NavBar"
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import sponge from '../assets/images/sponge.jpg'
 
 function LandingPage() {
 
     return (
         <>
-        <NavBar />
-        {/* TODO: add images and witty description of company and their services */}
-        {/* TODO: add footer component? */}
-        Tjo bror
+            <NavBar />
+            {/* TODO: add images and witty description of company and their services */}
+            {/* TODO: add footer component? */}
+            <p>Tjo bror</p>
+            <Container>
+                <Image src={sponge} fluid />
+            </Container>
         </>
     )
 }
