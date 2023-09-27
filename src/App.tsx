b216d12b5307f2c743de5914929f4f30e4042923
Angelina <linaemalmros@gmail.com>
2023-09-27 11:50:17 +0200

    landing page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
 
 function App() {
 
   return (
     <>
-{/*      <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/myPages" element={<MyPages />} />
+          {/*<Route path="/myPages" element={<MyPages />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />*/}
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter> 
     </>
   )
 }
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default LandingPage
