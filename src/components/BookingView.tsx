e2f7ca14b89236307cad9f31c42acce4199c592a
Angelina <linaemalmros@gmail.com>
2023-09-28 11:23:44 +0200

    moved the different cleaning services to own component for easier reuse

--- src/components/BookingView.tsx
+++ src/components/BookingView.tsx
@@ -1,15 +1,40 @@
 import NavBar from "../common/NavBar"
+import { useState, useEffect } from 'react'
+import TypesOfCleaning from "../common/TypesOfCleaing"
 
 const BookingView = () => {
-
+    const [ isPicked, setIsPicked ] = useState(false)
     const choice = sessionStorage.getItem('choice')
-    /* TODO: add logic for booking a service */
+
+    useEffect(() => {
+        const checkIfChoice = () => {
+            if (choice !== null) {
+                setIsPicked(true)
+            }
+        }
+
+        const interval = setInterval(() => {
+            checkIfChoice()
+        }, 1000)
+
+        checkIfChoice()
+
+        return () => clearInterval(interval)
+    }, [])
+
+    /* TODO: add logic with fetch for booking a service */
 
     return (
         <>
-        <NavBar />
-        <h1>BOOKING VIEW</h1>
-        <p>You want to book {choice}</p>
+            <NavBar />
+            <h1>BOOKING VIEW</h1>
+            {isPicked ?
+                <p>You want to book {choice}</p>
+                :
+                <TypesOfCleaning />
+            }
+
+            {/* TODO: add form for booking picked service */}
         </>
     )
 }
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,49 +1,12 @@
 import NavBar from "../common/NavBar";
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import { useState } from 'react'
+import TypesOfCleaning from "../common/TypesOfCleaing";
 
 function Services() {
-    const [choice, setChoice] = useState('')
-    const services = [
-        { title: 'Basic Städning', description: 'Grundläggande städning' },
-        { title: 'Topp Städning', description: 'Ingående städning' },
-        { title: 'Diamant Städning', description: 'Heltäckande städning' },
-        { title: 'Fönstertvätt', description: 'Cristal clear' }
-    ];
-
-    /* TODO: create logic for sending customer to booking view when clicking a service */
-
-    const handleChoice = (choice: string) => {
-        setChoice(choice)
-        sessionStorage.setItem('choice', choice)
-        
-        location.assign('/booking-view')
-    }
 
     return (
         <>
             <NavBar />
-            <Container className="mt-5">
-                <Row>
-                    {services.map((service, index) => (
-                        <Col md={3} key={index} className="mb-4">
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>{service.title}</Card.Title>
-                                    <Card.Text>{service.description}</Card.Text>
-                                    <Button
-                                        variant="outline-dark"
-                                        onClick={() => handleChoice(service.title)}>Välj</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
+            <TypesOfCleaning />
         </>
     );
 }
