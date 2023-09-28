15cf5e28bdf4564282f2fd2088a24d26c7d10f55
Joachim Persson <joachim.persson88@gmail.com>
2023-09-28 13:35:04 +0200

    jag har färdigställt denna filen, men jag håller fortfarande på med test-filen.

--- src/components/CancelCleaning.tsx
+++ src/components/CancelCleaning.tsx
@@ -2,36 +2,38 @@ import React, { useState } from 'react';
 
 function CancelCleaning() {
     const [showModal, setShowModal] = useState(false);
+    const [feedbackMessage, setFeedbackMessage] = useState('');
 
     const handleCancel = async () => {
 
         try {
           const response = await fetch('api', { method: 'POST' });
           if (response.ok) {
-              //Manage successful cancellation here, e.g. message to the user.
+              setFeedbackMessage("Städningen har framgångsrikt avbokats."); //Message for successful booking.
           } else {
-              //Handle cancellation errors here, e.g. display an error message.
+              const responseBody = await response.json();
+              setFeedbackMessage(responseBody.message || "Ett fel inträffade vid avbokningen."); //Message for failed booking.
           }
         } catch (error) {
           console.error('Ett fel inträffade:', error);
-          //Handle unexpected errors here, e.g. display an error message.
+            setFeedbackMessage("Ett oväntat fel inträffade. Försök igen senare."); //Message for unexpected error.
         }
 
-        setShowModal(false);
-        //After the cancellation process, hide the modal window.
+        setShowModal(false); //After the cancellation process, hide the modal window.
+
     };
 
     return (
         <div>
             <button onClick={() => setShowModal(true)}>Avboka städning</button>
-            {showModal && (
-                //If showModal is true, show the modal window.
+            {showModal && (  //If showModal is true, show the modal window.
                 <div>
                     Är du säker på att du vill avboka?
                     <button onClick={handleCancel}>Ja</button>
                     <button onClick={() => setShowModal(false)}>Nej</button>
                 </div>
             )}
+            {feedbackMessage && <p>{feedbackMessage}</p>}  {/* Show feedback message*/}
         </div>
     );
 }
