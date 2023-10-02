28d84e650898c71045989d331d76aeefba4c2a30
Joachim Persson <joachim.persson88@gmail.com>
2023-10-02 19:57:23 +0200

    Have put comments in these files on what each code does.

--- src/components/BookingManagement/ApprovedCleaning.tsx
+++ src/components/BookingManagement/ApprovedCleaning.tsx
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+//React functional component for displaying and handling approved cleanings.
 const ApprovedCleaning: React.FC = () => {
+    //Using React's useState hook to maintain a list of completed cleanings.
     const [completedCleanings, setCompletedCleanings] = useState<JobDto[]>([]);
 
+    //useEffect hook to run side effects, in this case, to fetch the list of completed cleanings when the component mounts.
     useEffect(() => {
-        //Assuming an endpoint for fetching completed cleanings.
+        //Making a GET request using axios to fetch completed cleanings.
         axios.get('http://localhost:8080/api/v1/job/completed-cleanings')
             .then((response) => {
+                //Updating the component's state with the fetched data.
                 setCompletedCleanings(response.data);
             })
             .catch((error) => {
+                // Logging an error if the request fails.
                 console.error("Error fetching data:", error);
             });
     }, []);
 
-    //Handler for approving a cleaning.
+    //Function to handle the approval of a cleaning.
     const approveCleaning = (id: string) => {
-        //Endpoint for approving a cleaning
+        //Making a PUT request to approve the cleaning by its ID.
         axios.put(`http://localhost:8080/api/v1/job/approve-cleaning/${id}`)
             .then(() => {
+                //Once approved, we remove the cleaning from our list (i.e., it's no longer in the 'completed' status).
                 setCompletedCleanings(prev => prev.filter(cleaning => cleaning.id !== id));
-                //Here you can also show some kind of success message or notification.
             })
             .catch((error) => {
+                //Logging an error if the request fails.
                 console.error("Error approving cleaning:", error);
             });
     };
 
+    //Rendering the component's JSX.
     return (
         <div className="container mt-4">
             <h1 className="text-center">Completed Cleanings</h1>
             {/*<Link to={`/booked-cleanings/${customerId}`}>See Booked Cleanings</Link>*/}
+
+            {/* Table to display the list of completed cleanings */}
             <table className="table table-bordered table-striped">
                 <thead className="thead-dark">
                 <tr>
@@ -42,12 +51,14 @@ const ApprovedCleaning: React.FC = () => {
                 </tr>
                 </thead>
                 <tbody>
+                {/* Renew through the completed cleanings and displays them in table rows. */}
                 {completedCleanings.map((cleaning) => (
                     <tr key={cleaning.id}>
                         <td>{cleaning.id}</td>
                         <td>{new Date(cleaning.bookedDate).toLocaleDateString()}</td>
                         <td>{cleaning.type}</td>
                         <td>
+                            {/* Button to approve a cleaning. When clicked, it invokes the approveCleaning function with the cleaning's ID */}
                             <button onClick={() => approveCleaning(cleaning.id)}>Approve</button>
                         </td>
                     </tr>
@@ -58,8 +69,10 @@ const ApprovedCleaning: React.FC = () => {
     );
 };
 
+//Exporting the component for use in other parts of the application.
 export default ApprovedCleaning;
 
+//TypeScript interface to define the structure of a job/cleaning.
 interface JobDto {
     id: string;
     bookedDate: string;
--- src/components/BookingManagement/UnitTest/ApprovedCleaning.test.tsx
+++ src/components/BookingManagement/UnitTest/ApprovedCleaning.test.tsx
@@ -2,28 +2,36 @@ import { render, waitFor, screen, act } from '@testing-library/react';
 import axios from 'axios';
 import ApprovedCleaning from "../ApprovedCleaning";
 
-// Mock axios requests
+//Mocking axios to avoid actual HTTP requests during testing.
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+//Resetting the mocked axios methods before each test to ensure a clean setup.
 beforeEach(() => {
     mockedAxios.get.mockReset();
     mockedAxios.put.mockReset();
 });
 
+//Grouping tests for the ApprovedCleaning component.
 describe('ApprovedCleaning Component', () => {
 
+    //Test for initial rendering of the component.
     it('should render without crashing', async () => {
+        //Mocking the GET request with an empty list.
         mockedAxios.get.mockResolvedValueOnce({ data: [] });
 
+        //Rendering the component inside an asynchronous act to handle state updates.
         await act(async () => {
             render(<ApprovedCleaning />);
         });
 
+        //Checking if the header text is present in the document.
         expect(screen.getByText('Completed Cleanings'));
     });
 
+    //Test for fetching and displaying the completed cleanings.
     it('should fetch and display completed cleanings', async () => {
+        //Mock data to simulate the fetched cleanings.
         const mockData = [{
             id: '1',
             bookedDate: '2023-09-28',
@@ -32,16 +40,22 @@ describe('ApprovedCleaning Component', () => {
             status: 'Completed',
         }];
 
+        //Mocking the GET request to return the mock data.
         mockedAxios.get.mockResolvedValueOnce({ data: mockData });
 
+        //Rendering the component.
         await act(async () => {
             render(<ApprovedCleaning />);
         });
 
+        //Expecting the axios GET method to have been called once.
         await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+
+        //Checking if the mock data's cleaning type is displayed.
         expect(screen.getByText('Basic Cleaning'));
     });
 
+    //Test to ensure that the component handles the approval of a cleaning.
     it('should handle approval of a cleaning', async () => {
         const mockData = [{
             id: '1',
@@ -52,33 +66,43 @@ describe('ApprovedCleaning Component', () => {
         }];
 
         mockedAxios.get.mockResolvedValueOnce({ data: mockData });
+        //Mocking the PUT request to simulate a successful approval.
         mockedAxios.put.mockResolvedValueOnce({});
 
         await act(async () => {
             render(<ApprovedCleaning />);
         });
 
+        //Simulating a button click to approve a cleaning.
         const approveButton = screen.getByText('Approve');
         act(() => {
             approveButton.click();
         });
 
+        //Expecting the axios PUT method to have been called with the correct endpoint.
         await waitFor(() => expect(axios.put).toHaveBeenCalledWith('http://localhost:8080/api/v1/job/approve-cleaning/1'));
     });
 
+    //Test to handle potential errors during the fetching of completed cleanings.
     it('should handle errors during fetching completed cleanings', async () => {
+        //Mocking a rejected GET request to simulate an error.
         mockedAxios.get.mockRejectedValueOnce(new Error('Failed to fetch data'));
 
-        console.error = jest.fn(); // Mocking console.error to check if the error is being logged
+        //Mocking console.error to check later if the error is being logged.
+        console.error = jest.fn();
 
         await act(async () => {
             render(<ApprovedCleaning />);
         });
 
+        //Expecting the axios GET method to have been called once.
         await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+
+        //Verifying that the error was logged to the console.
         expect(console.error).toHaveBeenCalledWith('Error fetching data:', expect.any(Error));
     });
 
+    //Test to handle potential errors during the approval of a cleaning.
     it('should handle errors during approval of a cleaning', async () => {
         const mockData = [{
             id: '1',
@@ -89,6 +113,7 @@ describe('ApprovedCleaning Component', () => {
         }];
 
         mockedAxios.get.mockResolvedValueOnce({ data: mockData });
+        //Mocking a rejected PUT request to simulate an error during approval.
         mockedAxios.put.mockRejectedValueOnce(new Error('Failed to approve cleaning'));
 
         await act(async () => {
@@ -100,7 +125,10 @@ describe('ApprovedCleaning Component', () => {
             approveButton.click();
         });
 
+        //Expecting the axios PUT method to have been called once.
         await waitFor(() => expect(axios.put).toHaveBeenCalledTimes(1));
+
+        //Verifying that the error was logged to the console.
         expect(console.error).toHaveBeenCalledWith('Error approving cleaning:', expect.any(Error));
     });
 });
