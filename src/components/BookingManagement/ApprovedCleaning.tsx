d1d8ef586e6945ceb13e7ab412d51ef70d88676c
Joachim Persson <joachim.persson88@gmail.com>
2023-10-03 11:20:58 +0200

    Updated this file.

--- src/components/BookingManagement/ApprovedCleaning.tsx
+++ src/components/BookingManagement/ApprovedCleaning.tsx
@@ -3,76 +3,88 @@ import axios from 'axios';
 
 //React functional component for displaying and handling approved cleanings.
 const ApprovedCleaning: React.FC = () => {
-    //Using React's useState hook to maintain a list of completed cleanings.
+    // States
     const [completedCleanings, setCompletedCleanings] = useState<JobDto[]>([]);
+    const [showModal, setShowModal] = useState(false);
+    const [feedback, setFeedback] = useState('');
+    const [selectedCleaning, setSelectedCleaning] = useState<JobDto | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
-    //useEffect hook to run side effects, in this case, to fetch the list of completed cleanings when the component mounts.
+    // Fetching data
     useEffect(() => {
-        //Making a GET request using axios to fetch completed cleanings.
         axios.get('http://localhost:8080/api/v1/job/completed-cleanings')
             .then((response) => {
-                //Updating the component's state with the fetched data.
                 setCompletedCleanings(response.data);
             })
             .catch((error) => {
-                // Logging an error if the request fails.
                 console.error("Error fetching data:", error);
+                setError('There was an error fetching the data.');
             });
     }, []);
 
-    //Function to handle the approval of a cleaning.
-    const approveCleaning = (id: string) => {
-        //Making a PUT request to approve the cleaning by its ID.
-        axios.put(`http://localhost:8080/api/v1/job/approve-cleaning/${id}`)
-            .then(() => {
-                //Once approved, we remove the cleaning from our list (i.e., it's no longer in the 'completed' status).
-                setCompletedCleanings(prev => prev.filter(cleaning => cleaning.id !== id));
+    const approveCleaning = () => {
+        if (selectedCleaning) {
+            axios.put(`http://localhost:8080/api/v1/job/approve-cleaning/${selectedCleaning.id}`, {
+                feedback: feedback
             })
-            .catch((error) => {
-                //Logging an error if the request fails.
-                console.error("Error approving cleaning:", error);
-            });
+                .then(() => {
+                    setCompletedCleanings(prev => prev.filter(cleaning => cleaning.id !== selectedCleaning.id));
+                    setShowModal(false);
+                    setFeedback('');
+                    setSelectedCleaning(null);
+                })
+                .catch((error) => {
+                    console.error("Error approving cleaning:", error);
+                    setError('There was an error approving the cleaning.');
+                });
+        }
     };
 
-    //Rendering the component's JSX.
+    // Rendering
     return (
         <div className="container mt-4">
             <h1 className="text-center">Completed Cleanings</h1>
-            {/*<Link to={`/booked-cleanings/${customerId}`}>See Booked Cleanings</Link>*/}
-
-            {/* Table to display the list of completed cleanings */}
+            {error && <p className="alert alert-danger">{error}</p>}
             <table className="table table-bordered table-striped">
                 <thead className="thead-dark">
                 <tr>
                     <th scope="col">Booking ID</th>
                     <th scope="col">Date</th>
-                    <th scope="col">Service Type</th>
+                        <th scope="col">Service Type</th>
                     <th scope="col">Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                {/* Renew through the completed cleanings and displays them in table rows. */}
                 {completedCleanings.map((cleaning) => (
                     <tr key={cleaning.id}>
                         <td>{cleaning.id}</td>
                         <td>{new Date(cleaning.bookedDate).toLocaleDateString()}</td>
                         <td>{cleaning.type}</td>
                         <td>
-                            {/* Button to approve a cleaning. When clicked, it invokes the approveCleaning function with the cleaning's ID */}
-                            <button onClick={() => approveCleaning(cleaning.id)}>Approve</button>
+                            <button onClick={() => {
+                                setSelectedCleaning(cleaning);
+                                setShowModal(true);
+                            }}>Approve</button>
                         </td>
                     </tr>
                 ))}
                 </tbody>
             </table>
+
+            {showModal && selectedCleaning && (
+                <div className="modal">
+                    <h2>Approve Cleaning for {selectedCleaning.id}</h2>
+                    <textarea value={feedback} onChange={(e) => setFeedback(e.target.value)} placeholder="Enter positive feedback..."></textarea>
+                    <button disabled={feedback.length === 0 || feedback.length > 1000} onClick={approveCleaning}>Submit Feedback</button>
+                    <button onClick={() => setShowModal(false)}>Close</button>
+                </div>
+            )}
         </div>
     );
 };
 
-//Exporting the component for use in other parts of the application.
 export default ApprovedCleaning;
 
-//TypeScript interface to define the structure of a job/cleaning.
 interface JobDto {
     id: string;
     bookedDate: string;
