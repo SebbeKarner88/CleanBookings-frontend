3767ed4a33044fb51480e1e99958214acccfc935
Angelina <linaemalmros@gmail.com>
2023-10-04 12:52:12 +0200

    Spellcheck

--- src/common/TypesOfCleaning.tsx
+++ src/common/TypesOfCleaning.tsx
@@ -7,7 +7,7 @@ const TypesOfCleaning = () => {
     const services = [
         { title: 'Basic Cleaning', description: 'Basic cleaning - vi dammsuger, våttorkar och dammar hela ditt hem. 500:-' },
         { title: 'Top Cleaning', description: 'Full cleaning - allt som ingår i basic plus avtorkning av kök och badrum. 1000:-' },
-        { title: 'Diamond Cleaing', description: 'Thorough cleaning with window cleaning included - allt som ingår i full plus grundlig rengöring av spis, ugn, microvågsugn, eventuell tvättmaskin samt fönstertvätt. 1500:-' },
+        { title: 'Diamond Cleaning', description: 'Thorough cleaning with window cleaning included - allt som ingår i full plus grundlig rengöring av spis, ugn, microvågsugn, eventuell tvättmaskin samt fönstertvätt. 1500:-' },
         { title: 'Window Cleaning', description: 'Putsning av alla fönster i fastigheten. Från 500:-' }
     ]
 
