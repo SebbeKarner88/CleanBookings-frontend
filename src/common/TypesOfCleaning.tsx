dff2385460df0ca47f13debd60f46af54b428047
SebastianKärner88 <sebastiankarner@live.se>
2023-10-09 10:38:54 +0200

    changed prices on cleanings

--- src/common/TypesOfCleaning.tsx
+++ src/common/TypesOfCleaning.tsx
@@ -5,10 +5,10 @@ import Col from 'react-bootstrap/Col';
 
 const TypesOfCleaning = () => {
     const services = [
-        { title: 'Basic Cleaning', description: 'Basic cleaning - vi dammsuger, våttorkar och dammar hela ditt hem. 500:-' },
-        { title: 'Top Cleaning', description: 'Full cleaning - allt som ingår i basic plus avtorkning av kök och badrum. 1000:-' },
-        { title: 'Diamond Cleaning', description: 'Thorough cleaning with window cleaning included - allt som ingår i full plus grundlig rengöring av spis, ugn, microvågsugn, eventuell tvättmaskin samt fönstertvätt. 1500:-' },
-        { title: 'Window Cleaning', description: 'Putsning av alla fönster i fastigheten. Från 500:-' }
+        { title: 'Basic Cleaning', description: 'Basic cleaning - vi dammsuger, våttorkar och dammar hela ditt hem. 795:-' },
+        { title: 'Top Cleaning', description: 'Full cleaning - allt som ingår i basic plus avtorkning av kök och badrum. 1495:-' },
+        { title: 'Diamond Cleaning', description: 'Thorough cleaning with window cleaning included - allt som ingår i full plus grundlig rengöring av spis, ugn, microvågsugn, eventuell tvättmaskin samt fönstertvätt. 2495:-' },
+        { title: 'Window Cleaning', description: 'Putsning av alla fönster i fastigheten. Från 495:-' }
     ]
 
     return (
