41c3afafd57beb374bb7666b4cf85ee4f680900a
SebastianKärner88 <sebastiankarner@live.se>
2023-10-18 10:39:41 +0200

    Footer design

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,5 +1,5 @@
 import './Footer.css'
-import { FaPhone, FaMobile, FaMapMarker, FaEnvelopeSquare } from 'react-icons/fa'
+import {FaPhone, FaMobile, FaMapMarker, FaEnvelopeSquare} from 'react-icons/fa'
 import styled from 'styled-components'
 
 export const Footer = () => {
@@ -8,17 +8,19 @@ export const Footer = () => {
         <>
             <div className="container-fluid">
                 <div className="row  row-no-gutters">
-                    <div className="col-xs-12 col-sm-6 col-md-3" >
+                    <div className="col-xs-12 col-sm-6 col-md-3">
                         <ul>
                             <li className="col-heading">Contact</li>
-                            <li><Phone /> <a href="tel:0707000000">555-CLEAN-ME-NOW</a></li>
-                            <li><Mobile /> <a href="sms:0707000000">070-CLEAN-ME-NOW</a></li>
-                            <li><Map /> <a href="https://maps.app.goo.gl/HzU8kv5sVTQKF5cVA">Stockholm HQ</a></li>
-                            <li><Envelope /> <a href="mailto:bokning@Cleanbookings.com?subject=Mail Hemsida">booking@cleanbookings.com</a></li>
+                                <li className="text-start"><Phone/> <a href="tel:0707000000">555-CLEAN-ME-NOW</a></li>
+                                <li className="text-start"><Mobile/> <a href="sms:0707000000">070-CLEAN-ME-NOW</a></li>
+                                <li className="text-start"><Map/> <a href="https://maps.app.goo.gl/HzU8kv5sVTQKF5cVA">Stockholm HQ</a></li>
+                                <li className="text-start"><Envelope/> <a
+                                    href="mailto:bokning@Cleanbookings.com?subject=Mail Hemsida">booking@cleanbookings.com</a>
+                                </li>
                         </ul>
                     </div>
 
-                    <div className="col-xs-12 col-sm-6 col-md-3" >
+                    <div className="col-xs-12 col-sm-6 col-md-3">
                         <ul>
                             <li className="col-heading">Locations</li>
                             <li><a href="https://maps.app.goo.gl/5xnZx6c4h5FFELxp8">Stockholm</a></li>
@@ -28,7 +30,7 @@ export const Footer = () => {
                         </ul>
                     </div>
 
-                    <div className="col-xs-12 col-sm-6 col-md-3" >
+                    <div className="col-xs-12 col-sm-6 col-md-3">
                         <ul>
                             <li className="col-heading">Our Work</li>
                             <li><a href="#">Städafint's cleaning products</a></li>
@@ -38,7 +40,7 @@ export const Footer = () => {
                         </ul>
                     </div>
 
-                    <div className="col-xs-12 col-sm-6 col-md-3" >
+                    <div className="col-xs-12 col-sm-6 col-md-3">
                         <ul>
                             <li className="col-heading">Information</li>
                             <li><a href="#">GDPR</a></li>
@@ -49,9 +51,9 @@ export const Footer = () => {
                     </div>
                 </div>
 
-                <div className="row row-no-gutters" id="bottom-footer" >
+                <div className="row row-no-gutters" id="bottom-footer">
 
-                    <div className="col-xs-12 col-md-5 text-center" >
+                    <div className="col-xs-12 col-md-5 text-center">
                         <ul className="vertical-links small">
                             <li><a href="#">Privacy</a></li>
                             <li><a href="#">Terms & Conditions</a></li>
@@ -59,12 +61,14 @@ export const Footer = () => {
                             <li><a href="#">Homepage design</a></li>
                         </ul>
                     </div>
-                    <div className="col-xs-12 col-md-2 text-center" >
+                    <div className="col-xs-12 col-md-2 text-center">
                         <p><i className="fa fa-heart-o" aria-hidden="true"></i></p>
                     </div>
-                    <div className="col-xs-12 col-md-5 text-center" >
+                    <div className="col-xs-12 col-md-5 text-center">
                         <ul>
-                            <li className="small">© Copyright 2023 Website by <a href="#">CodeCrafters</a>. All Rights reserved.</li>
+                            <li className="small">© Copyright 2023 Website by <a href="#">CodeCrafters</a>. All Rights
+                                reserved.
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -81,22 +85,22 @@ const Phone = styled(FaPhone)`
 `;
 
 const Mobile = styled(FaMobile)`
-color: #304f50;
-margin-right: 10px;
-font-size: 18px;
-width: 2.25rem;
+  color: #304f50;
+  margin-right: 10px;
+  font-size: 18px;
+  width: 2.25rem;
 `;
 
 const Map = styled(FaMapMarker)`
-color: #304f50;
-margin-right: 10px;
-font-size: 18px;
-width: 2.25rem;
+  color: #304f50;
+  margin-right: 10px;
+  font-size: 18px;
+  width: 2.25rem;
 `;
 
 const Envelope = styled(FaEnvelopeSquare)`
-color: #304f50;
-margin-right: 10px;
-font-size: 18px;
-width: 2.25rem;
+  color: #304f50;
+  margin-right: 10px;
+  font-size: 18px;
+  width: 2.25rem;
 `;
