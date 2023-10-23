71b57aa8a23d20fb9cb09a5a639e2a785c2df750
Angelina <linaemalmros@gmail.com>
2023-10-23 13:13:07 +0200

    Styling

--- src/components/services-view/TypesOfCleaning.tsx
+++ src/components/services-view/TypesOfCleaning.tsx
@@ -8,11 +8,11 @@ import CleaningCard from "./CleaningCard.tsx";
 const TypesOfCleaning = () => {
     const services = [
         {
-            image: 'basic',
-            title: 'BASIC',
-            description: 'Basic Städning. Det mest prisvärda alternativet, Storsäljaren.',
-            items: ['Dammsugning av alla ytor', 'Dammtorka möbler', 'Våttorka golv'],
-            price: '795kr'
+            image: 'diamond',
+            title: 'DIAMANT',
+            description: 'Vår 100% steril garantistäd\nPerfekt för dig som ska utföra en operation i vardagsrummet.',
+            items: ['Dammsugning av alla ytor', 'Dammtorka möbler', 'Våttorka golv', 'Städning av kök', 'Välta kon', 'Rensa fisken' ],
+            price: '2495kr'
         },
         {
             image: 'top',
@@ -22,17 +22,17 @@ const TypesOfCleaning = () => {
             price: '1495kr'
         },
         {
-            image: 'diamond',
-            title: 'DIAMANT',
-            description: 'Vår 100% steril garantistäd\nPerfekt för dig som ska utföra en operation i vardagsrummet.',
-            items: ['Dammsugning av alla ytor', 'Dammtorka möbler', 'Våttorka golv', 'Städning av kök', 'Välta kon', 'Rensa fisken' ],
-            price: '2495kr'
+            image: 'basic',
+            title: 'BASIC',
+            description: 'Basic Städning. Det mest prisvärda alternativet, Storsäljaren.',
+            items: ['Dammsugning av alla ytor', 'Dammtorka möbler', 'Våttorka golv'],
+            price: '795kr'
         },
         {
             image: 'window',
             title: 'FÖNSTER',
             description: 'En klassisk fönstertvätt.',
-            items: ['Dammsugning av alla ytor', 'Dammtorka möbler', 'Våttorka golv'],
+            items: [],
             price: '795kr'
         }
     ]
@@ -42,7 +42,7 @@ const TypesOfCleaning = () => {
             <Container className="pt-5 px-5">
                 <Row xs={1} md={2} lg={4}>
                     {services.map((service, index) => (
-                        <Col key={index} className="mb-5">
+                        <Col key={index} className="mb-5 d-flex align-items-stretch">
                             <CleaningCard
                                 image={service.image}
                                 title={service.title}
