3482c4c78e097826124fa45b99375132ef3714a5
@jimmy.brissman <jimmy.brissman@protonmail.com>
2023-11-09 09:33:37 +0100

    REFACTOR: CheckoutView
    Added some styling to the view.

--- src/components/CheckoutView.tsx
+++ src/components/CheckoutView.tsx
@@ -1,13 +1,23 @@
 import {useLocation} from "react-router-dom";
+import NavBar from "../common/NavBar.tsx";
+import {Footer} from "../common/Footer.tsx";
 
 export default function CheckoutView() {
     const location = useLocation();
     return (
-        <iframe
-            width={screen.width}
-            height={screen.height}
-            style={{maxWidth: "100vw", maxHeight: "100vh"}}
-            srcDoc={location.state.snippet}
-        ></iframe>
+        <>
+            <NavBar/>
+            <div className="bg-image min-vw-100 d-flex align-items-center py-4">
+                <div className="bg-white p-md-4 rounded rounded-4 my-md-4 m-auto">
+                    <iframe
+                        width={800}
+                        height={screen.height}
+                        style={{maxWidth: "100vw", maxHeight: "100vh"}}
+                        srcDoc={location.state.snippet}
+                    ></iframe>
+                </div>
+            </div>
+            <Footer/>
+        </>
     );
 }
