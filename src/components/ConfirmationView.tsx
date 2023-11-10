34d0d0d5534132ee7f80a2b85d32b2eed8cc39f2
@jimmy.brissman <jimmy.brissman@protonmail.com>
2023-11-10 11:30:57 +0100

    REFACTOR: ConfirmationView
    Removed the AuthContext from the confirmation-view since the iframe doesn't seem to have access to this data anyway. Now it just renders a generic view to indicate a successful request.

--- src/components/ConfirmationView.tsx
+++ src/components/ConfirmationView.tsx
@@ -1,22 +1,18 @@
-import {useContext} from "react";
-import {AuthContext} from "../context/AuthContext.tsx";
-
 export default function ConfirmationView() {
-    const {name, username} = useContext(AuthContext);
 
     return (
         <div className="container">
             <h1 className="text-md-center fw-bold my-4">
-                Tack för din bokning {name}!
+                Tack för din bokning!
             </h1>
             <img
                 className={"my-4"}
                 src="/src/assets/images/check.png"
-                alt="A checkbox indicating success"
+                alt="A checkbox indicating successful request"
                 width={300}
             />
-            <p className={"my-4 text-start"}>Du kommer snart att få en bekräftelse på din bokade städning skickad till
-                <span className="fw-bold"> {username}</span>
+            <p className={"my-4 text-start"}>
+                Du kommer snart att få en bekräftelse på din bokade städning skickad till din e-postadress.
             </p>
         </div>
     )
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), basicSsl()],
   server: {
     port: 5173
   }
